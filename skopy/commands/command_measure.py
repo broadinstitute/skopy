a15fa002b1396a0d970bcffddd10a862ff8601b0
Allen Goodman <allen.goodman@icloud.com>
2017-05-10 17:20:13 -0400

    distribute option

--- skopy/commands/command_measure.py
+++ skopy/commands/command_measure.py
@@ -1,3 +1,5 @@
+import os.path
+
 import click
 import pandas
 import sqlalchemy
@@ -5,24 +7,33 @@ import sqlalchemy.orm
 
 import skopy.command
 import skopy.feature
+import skopy.task
 
 
 @click.command("measure")
 @click.argument("metadata", nargs=1, type=click.Path(exists=True))
 @click.option("--database", default="sqlite:///measurements.sqlite")
+@click.option("--distribute", is_flag=True)
 @click.option("--verbose", is_flag=True)
-def command(metadata, database, verbose):
-    engine = sqlalchemy.create_engine(database, echo=verbose)
+def command(metadata, database, distribute, verbose):
+    if distribute:
+        pass
+    else:
+        engine = sqlalchemy.create_engine(database, echo=verbose)
 
-    skopy.feature.Base.metadata.drop_all(engine)
+        skopy.feature.Base.metadata.drop_all(engine)
 
-    skopy.feature.Base.metadata.create_all(engine)
+        skopy.feature.Base.metadata.create_all(engine)
 
-    session = sqlalchemy.orm.sessionmaker()
+        session = sqlalchemy.orm.sessionmaker()
 
-    session.configure(bind=engine)
+        session.configure(bind=engine)
 
-    session = session()
+        session = session()
+
+    directory = os.path.dirname(metadata)
+
+    directory = os.path.abspath(directory)
 
     metadata = pandas.read_csv(metadata)
 
@@ -30,8 +41,15 @@ def command(metadata, database, verbose):
 
     with progress as metadata:
         for pathname, mask in metadata:
-            image = skopy.feature.extract(pathname, mask)
+            pathname = os.path.join(directory, pathname)
 
-            session.add(image)
+            mask = os.path.join(directory, mask)
 
-    session.commit()
+            if distribute:
+                skopy.task.measure.delay(pathname, mask)
+            else:
+                image = skopy.feature.extract(pathname, mask)
+
+                session.add(image)
+
+                session.commit()
--- skopy/task.py
+++ skopy/task.py
@@ -1,28 +1,62 @@
-import celery
+import celery.bin
+import celery.bootsteps
 import sqlalchemy.orm
 
 import skopy.command
 import skopy.feature
 
-queue = celery.Celery("tasks", backend="rpc://", broker="amqp://localhost")
-
-engine = sqlalchemy.create_engine("postgresql+psycopg2://allen@localhost/skopy-test", echo=True)
-
-skopy.feature.Base.metadata.drop_all(engine)
-
-skopy.feature.Base.metadata.create_all(engine)
-
-session = sqlalchemy.orm.sessionmaker()
-
-session.configure(bind=engine)
-
-session = session()
+application = celery.Celery("tasks", backend="rpc://", broker="amqp://localhost")
 
 
-@queue.task
-def measure(pathname, mask):
+class Task(celery.Task):
+    def __init__(self):
+        self.database = "postgresql+psycopg2://allen@localhost/skopy-test"
+
+        self.echo = False
+
+        self._session = None
+
+    @property
+    def session(self):
+        if self._session is None:
+            engine = sqlalchemy.create_engine(self.database, echo=self.echo)
+
+            skopy.feature.Base.metadata.drop_all(engine)
+
+            skopy.feature.Base.metadata.create_all(engine)
+
+            session = sqlalchemy.orm.sessionmaker()
+
+            session.configure(bind=engine)
+
+            self._session = session()
+
+        return self._session
+
+    def run(self, *args, **kwargs):
+        super(Task, self).run(*args, **kwargs)
+
+
+application.user_options["worker"].add(celery.bin.Option("--database", dest="database", default=None))
+
+application.user_options["worker"].add(celery.bin.Option("--echo", dest="echo", default=False))
+
+
+class Step(celery.bootsteps.Step):
+    def __init__(self, parent, database, echo, **kwargs):
+        Task.database = database
+
+        Task.echo = echo
+
+        super(Step, self).__init__(parent, **kwargs)
+
+application.steps["worker"].add(Step)
+
+
+@application.task(base=Task, bind=True)
+def measure(self, pathname, mask):
     image = skopy.feature.extract(pathname, mask)
 
-    session.add(image)
+    self.session.add(image)
 
-    session.commit()
+    self.session.commit()
