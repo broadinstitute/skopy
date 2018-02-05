eeb28499d6505c57dc8e1541affa2d8c92fa0332
Allen Goodman <allen.goodman@icloud.com>
2018-02-05 16:37:57 -0500

    additional cleanup

--- skopy/commands/command_measure.py
+++ skopy/commands/command_measure.py
@@ -13,10 +13,10 @@ import skopy.feature_extraction.image
 @click.command("measure")
 @click.argument("metadata", nargs=1, type=click.Path(exists=True))
 @click.option("--output", "-o", nargs=1, type=click.Path())
-def command(metadata, output_pathname):
-    if output_pathname:
+def command(metadata, output):
+    if output:
         with contextlib.suppress(FileNotFoundError):
-            os.remove(output_pathname)
+            os.remove(output)
 
     metadata = pandas.read_csv(metadata)
 
@@ -24,22 +24,22 @@ def command(metadata, output_pathname):
 
     progress = click.progressbar(
         [
-            (image_pathname, mask_pathname, description)
-            for _, image_pathname, mask_pathname, description in metadata
+            (image_pathname, mask_pathname)
+            for _, image_pathname, mask_pathname in metadata
         ]
     )
 
     with progress as metadata:
-        for image_pathname, mask_pathname, description in metadata:
+        for image_pathname, mask_pathname in metadata:
             image = skimage.io.imread(image_pathname)
 
             mask = skimage.io.imread(mask_pathname)
 
             features = skopy.feature_extraction.image.extract_object_features(image, mask)
 
-            if os.path.exists(output_pathname):
-                with open(output_pathname, "a") as fd:
+            if os.path.exists(output):
+                with open(output, "a") as fd:
                     features.to_csv(fd, header=False, index=False)
             else:
-                with open(output_pathname, "w") as fd:
+                with open(output, "w") as fd:
                     features.to_csv(fd, header=True, index=False)
